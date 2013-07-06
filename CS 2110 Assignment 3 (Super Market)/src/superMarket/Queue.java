8787f3da3e5f5f8e25688d00bb6d67a2417cc65f
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-07-06 14:15:40 -0400

    Queue Class

--- CS 2110 Assignment 3 (Super Market)/src/superMarket/Queue.java
+++ CS 2110 Assignment 3 (Super Market)/src/superMarket/Queue.java
@@ -2,4 +2,23 @@ package superMarket;
 
 public class Queue {
 	String h="hello";
-}
+private Node header,current;
+private int length;
+
+public void join (Person p) throws BadInsertionPoint {
+	if (curretn == null) throw new BadInsertionPoint();
+	Node temp == new Node (p,current.getNext());
+	current.setNext(temp);
+	length++;
+}//end join method
+
+public void leave() {
+	if (!isEmpty() && inList()){
+		getPrevious ().setNext (current.getNext());
+		length--;
+	}//end if
+	else System.out.println("Sorry, list is empty.");
+}// end leave method
+
+
+}//end class Queue
