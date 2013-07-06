5d7a01914a5b640c1e86ae90f93014ea8e191de3
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-07-06 14:36:15 -0400

    Finished Queue Class

--- CS 2110 Assignment 3 (Super Market)/src/superMarket/Queue.java
+++ CS 2110 Assignment 3 (Super Market)/src/superMarket/Queue.java
@@ -20,5 +20,41 @@ public void leave() {
 	else System.out.println("Sorry, list is empty.");
 }// end leave method
 
+private Node getPrevious () {
+	for (Node n=header;n!=null && n.getNext() != null; n=n.getNext()){
+		if(n.getNext() == current) {return n;}//close if loop
+		return null;
+	}//end for loop
+		
+}// this return is only reached if no 'previous' was found
+
+public void setCurrent ( Person p ) { // sets ‘current’ to point to where p is
+	for ( current = header ; current != null ; current = current . getNext( ) )
+		if ( current.getData ( ).equals ( p ) ) return ; 
+}//close setCurrent mutator method
+
+public boolean inList ( ) {
+	return ( current != null && current != header ) ; 
+}
+
+public int getLength ( ) {
+	return this . length ; 
+}//accessory method
+
+public boolean isFull ( ) {
+	return false ; 
+}//This can never be full!
+
+public boolean isEmpty ( ) { 
+	return ( length == 0 ) ; 
+}//Returns true if queue list is empty
+
+public Queue ( int n ) {
+	header = new Node ( ) ;
+	current = header; length = 0 ; 
+}
+public Queue () {
+	this (0);
+}//Null Constructor
 
 }//end class Queue
