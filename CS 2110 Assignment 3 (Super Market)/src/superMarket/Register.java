e8e56cd369a6b1fb5f33fca6b4a79d7734ffaf2d
Ishtyaq Habib <ishtyaq.habib@gmail.com>
2013-07-06 23:03:45 -0400

    Using ArrayList

--- CS 2110 Assignment 3 (Super Market)/src/superMarket/Register.java
+++ CS 2110 Assignment 3 (Super Market)/src/superMarket/Register.java
@@ -1,5 +1,29 @@
 package superMarket;
 
-public class Register {
+import java.util.ArrayList;
 
+public class Register {
+	private ArrayList<Object> q;
+	public Register() {
+		q = new ArrayList<Object>();
+	}//Constructor
+	
+	//Put into Queue Class
+	public void enterLine(Person p){//someone enters the line
+		q.add(p);
+	}
+	
+	public void serve() {//Cashier serves first person
+		if(!q.isEmpty()) {
+			q.remove(0);
+		}
+	}
+	
+	public void leave(Person p) {//Person leaves line
+		q.remove(q.indexOf(p));
+	}
+	
+	public String toString(){
+		return q.toString();
+	}
 }
--- CS 2110 Assignment 3 (Super Market)/src/superMarket/SuperMarket.java
+++ CS 2110 Assignment 3 (Super Market)/src/superMarket/SuperMarket.java
@@ -2,30 +2,26 @@ package superMarket;
 
 public class SuperMarket {
 	public static void main(String args[]) throws Exception {
-		Queue q = new Queue();
+		Register bob = new Register();
 		Person p = new Person();
 		Person r = new Person();
 		Person w = new Person();
 		Person e = new Person();
 		Person y = new Person();
 		Person t = new Person();
-		q.join(p);
-		System.out.println(q.toString());
-		q.join(r);
-		System.out.println(q);
-		q.join(w);
-		q.join(e);
-		q.join(y);
-		q.join(t);
-		q.setCurrent(p);
-		System.out.println("Current: "+q.getCurrent());
-		System.out.println("Header: "+q.getHeader());
-		System.out.println("after six people: "+q);
-		q.leave();
-		System.out.println(q.getCurrent());
-		System.out.println("Current: "+q.getCurrent());
-		System.out.println("Header: "+q.getHeader());
-		System.out.println("after leave: "+q);
+		bob.enterLine(p);
+		System.out.println(bob.toString());
+		bob.enterLine(r);
+		System.out.println(bob);
+		bob.enterLine(w);
+		bob.enterLine(e);
+		bob.enterLine(y);
+		bob.enterLine(t);
+		System.out.println("after six people: "+bob);
+		bob.serve();
+		System.out.println("after serveing: "+bob);
+		bob.leave(y);
+		System.out.println("after leave: "+bob);
 		
 	}//Close main method
 	
